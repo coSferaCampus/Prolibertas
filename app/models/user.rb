aa58bacdcc9f29dd59c9b48ddc03a834aa3fa405
Eloy GL <eloydw@gmail.com>
2015-03-16 11:24:50 +0100

    Configurado Devise

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User
+  include Mongoid::Document
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :rememberable, :trackable
+
+  ## Database authenticatable
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  field :sign_in_count,      type: Integer, default: 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+
+
+end
