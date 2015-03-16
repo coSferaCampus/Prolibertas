ce9a307543e4eaceb58ee99e680380a0745c1243
Javier Montero <montero1990@hotmail.com>
2015-03-16 13:15:15 +0100

    Creado el estado personas y configurado como estado por defecto.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,6 +3,13 @@
 
   // Config
   app.config(function($urlRouterProvider, $stateProvider, $httpProvider) {
+  	// Para las urls que no se encuentren, redirigimos a la raíz.
+ 	 $urlRouterProvider.otherwise("/personas");
+  	// Aquí establecemos los estados de nuestra applicación.
+	  $stateProvider
+	  .state("personas", {
+	    url: "/personas",
+	    templateUrl: "personas.html"
+  	});
   });
-
 })();
