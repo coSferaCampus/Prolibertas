55865edf5fdb1e6325fb828f7994181b89fb2de2
illescas <illescas@srcorrecto.com>
2015-03-16 13:03:53 +0100

    creado el modelo de datos (person) con su test en verde correspondiente

---
+++ app/models/person.rb
@@ -0,0 +1,27 @@
+class Person 
+	include Mongoid::Document
+	include Mongoid::Timestamps
+
+	field :name,       		type: String
+	field :surname,    		type: String
+	field :origin,  		type: String
+	field :genre,   		type: Symbol
+	field :phone,    		type: String
+	field :assistance, 		type: String
+	field :dwelling,  		type: String
+	field :family_status,   type: String
+	field :health_status, 	type: String
+	field :birth,  			type: Date
+	field :nif,    			type: String
+	field :social_services, type: String
+	field :menu,  			type: String
+	field :income,  		type: String
+	field :address,    		type: String
+	field :contact_family, 	type: String
+	field :notes,  			type: String
+
+	validates :name, presence: true
+	validates :surname, presence: true
+	validates :genre, inclusion: {in: [:man, :woman]}
+end
+
