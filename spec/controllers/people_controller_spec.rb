82b3b6c5d0ba03bad5f84e586a631825fe641f08
illescas <illescas@srcorrecto.com>
2015-03-17 13:22:15 +0100

    implementado autenticación de usuario en el controlador people

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe PeopleController, type: :controller do
   set_content_type 'application/json'
 
   options = [:show, :index, :create, :update, :destroy]
-  options = [:show, :index, :create, :update]
   json_attributes = FactoryGirl.attributes_for(:person).keys
 
   before :all do
+    @user = FactoryGirl.create(:user)
     # Para todos los tests
     @model = Person
 
     # Para el test de show
     @resource = FactoryGirl.create(:person)
 
-  # Para el test de index
+    # Para el test de index
     @first_page_resources = Person.all
 
 
@@ -27,6 +27,9 @@ RSpec.describe PeopleController, type: :controller do
 
   end
 
+  before do
+    sign_in @user
+  end
 
   it_behaves_like "a REST controller", options, json_attributes
 
