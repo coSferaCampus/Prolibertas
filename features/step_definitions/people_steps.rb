fe342de36ac4507b498feb3e5a17ff2da55c1e39
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-19 10:49:16 +0100

    Cambiados person.feature person_step.rb a las carpetas de people

--- features/step_definitions/people_steps.rb
+++ features/step_definitions/people_steps.rb
@@ -75,3 +75,13 @@ Then(/^I should see the list of the people with "(.*?)" and "(.*?)" as surname a
   end
 end
 
+# PERSON
+
+When(/^I click the view icon of a person in people list view$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should go to a view of this person$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
--- features/step_definitions/person_steps.rb
+++
@@ -1,7 +0,0 @@
-When(/^I click the view icon of a person in people list view$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should go to a view of this person$/) do
-  pending # express the regexp above with the code you wish you had
-end
