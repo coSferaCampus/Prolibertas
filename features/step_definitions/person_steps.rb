d58ef979b4b400acffb03ab2adf3e2b0d0aca638
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-19 10:37:38 +0100

    Cambiado person.feature de carpeta y creado el person_steps.rb base

---
+++ features/step_definitions/person_steps.rb
@@ -0,0 +1,7 @@
+When(/^I click the view icon of a person in people list view$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should go to a view of this person$/) do
+  pending # express the regexp above with the code you wish you had
+end
