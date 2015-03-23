16044e98a8f39dfacbdb6952aa731038cdb90df8
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-23 18:26:54 +0100

    quitadas autorizaciones a los servicios

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,14 +3,11 @@ class ServicesController < ApplicationController
 
   def show
     @service = Service.find(params[:id])
-    authorize! :show, @service
     respond_with @service
   end
 
   def index
     @services = Service.all
-    authorize! :index, @services
     respond_with @services
   end
-
 end
