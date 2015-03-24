01d450fe5871f46836bcb074d935ee754fdc2700
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-24 13:21:10 +0100

    Añadidos al test del modelo usuario los campos  :full_name, :email y :tlf

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,8 @@ RSpec.describe User, type: :model do
 
   context "Fields" do
     it { is_expected.to have_field(:name).of_type(String) }
+    it { is_expected.to have_field(:full_name).of_type(String) }
+    it { is_expected.to have_field(:email).of_type(String) }
+    it { is_expected.to have_field(:tlf).of_type(String) }
   end
 end
