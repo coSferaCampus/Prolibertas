58951f6577b3884656dbfa565e22c6547d2f9443
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-24 17:35:35 +0100

    Create tests for volunteer restrictions about User

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,16 +5,8 @@ class Ability
     can     :manage, User
     can     :manage, Person
 
-
-    #if user.has_role? :director
-    #  can     :manage, User
-    #  can     :manage, Person
-    #elsif user.has_role? :worker
-    #  can     :manage, User
-    #  can     :manage, Person
-    #  cannot  :write, User
     if user.has_role? :volunteer
-      cannot  :create, User
+      cannot  :manage, User
     end
   end
 end
