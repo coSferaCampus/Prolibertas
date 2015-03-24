ebb164171869ebe144e44eb51dc473fd95bf552c
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-24 17:15:02 +0100

    Create user forbidden for volunteer

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, User
-    can :manage, Person
+    can     :manage, User
+    can     :manage, Person
+
+
+    #if user.has_role? :director
+    #  can     :manage, User
+    #  can     :manage, Person
+    #elsif user.has_role? :worker
+    #  can     :manage, User
+    #  can     :manage, Person
+    #  cannot  :write, User
+    if user.has_role? :volunteer
+      cannot  :create, User
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe UsersController, type: :controller do
   json_attributes = [:name, :full_name, :email, :tlf]
 
   before :all do
-    @user = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:director)
+    @worker = FactoryGirl.create(:worker)
+    @volunteer = FactoryGirl.create(:volunteer)
 
     # Para todos los tests
     @model = User
@@ -33,4 +35,20 @@ RSpec.describe UsersController, type: :controller do
   end
 
   it_behaves_like "a REST controller", options, json_attributes
+
+  context "abilities" do 
+
+    context "volunteer" do
+      before do
+        sign_in @volunteer
+      end
+
+      context "POST #create" do
+        it "returns 403 HTTP status code" do
+          post :create, user: @parameters 
+          expect(response).to have_http_status :forbidden
+        end
+      end
+    end
+  end
 end
