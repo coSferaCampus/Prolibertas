101d5b7005c0fe5507857bbe5cc68159c412912e
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-24 17:15:02 +0100

    Create user forbidden for volunteer

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,20 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
     can :manage, User
     can :manage, Person
     can :manage, Alert
+
+    #if user.has_role? :director
+    #  can     :manage, User
+    #  can     :manage, Person
+    #elsif user.has_role? :worker
+    #  can     :manage, User
+    #  can     :manage, Person
+    #  cannot  :write, User
+    if user.has_role? :volunteer
+      cannot  :create, User
+    end
   end
 end
