28b8ef6fca960162d3487ef5af68e53e154cccb9
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-03-24 13:11:21 +0100

    Añadidos a usuario los campos  :full_name, :email y :tlf

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -2,4 +2,16 @@ FactoryGirl.define do
   sequence :name do |n|
     "nombre#{n}"
   end
+
+  sequence :full_name do |n|
+    "nombre#{n} apellido#{n}"
+  end
+
+  sequence :email do |n|
+    "email#{n}@email.com"
+  end
+
+  sequence :tlf do |n|
+    "957" + "%06d" % n
+  end
 end
