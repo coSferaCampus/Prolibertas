45597a2ba82f2c8d4ca0aef9b5fee5ae5daabb64
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2015-04-01 14:56:36 +0200

    Los usuarios worker pueden verse a si mismos pero no editarse(a exepcion de la password) ni eliminarse

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,8 +17,12 @@ class Ability
        usuario.has_role? :director
       end
     elsif user.has_role? :worker
-      cannot [:show, :destroy], User do |usuario|
+      cannot :destroy, User do |usuario|
         usuario.has_role? :worker or usuario.has_role? :director
+      end
+
+      cannot :show, User do |usuario|
+        (usuario.has_role? :worker and user != usuario) or usuario.has_role? :director
       end
     elsif user.has_role? :volunteer
       cannot [:index, :create, :update, :destroy], User
