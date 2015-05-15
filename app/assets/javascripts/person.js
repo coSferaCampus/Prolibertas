502bea9280bbe17da4b0a3ff2e47463872f5ee9b
isabel <isgon22@gmail.com>
2015-05-15 12:03:03 +0200

    añadida edad en listado de personas

--- app/assets/javascripts/person.js
+++ app/assets/javascripts/person.js
@@ -26,6 +26,35 @@
         scope.services = data.services;
       });
 
+  //función para calcular la edad a partir de la fecha de nacimiento
+   scope.anos = function (birth){
+    var e = birth.split("-");
+ 
+    var ano =  e[0];
+    var mes =  e[1];
+    var dia =  e[2];
+
+    console.log({ano});
+    
+    fecha_hoy = new Date();
+    ahora_ano = fecha_hoy.getYear();
+    ahora_mes = fecha_hoy.getMonth();
+    ahora_dia = fecha_hoy.getDate();
+    edad = (ahora_ano + 1900) - ano;
+    
+      if ( ahora_mes < (mes - 1)){
+        edad--;
+     }
+     if (((mes - 1) == ahora_mes) && (ahora_dia < dia)){ 
+       edad--;
+     }
+     if (edad > 1900){
+       edad -= 1900;
+     }
+
+      return edad;
+}
+
     scope.genero = function(genero){
       if (genero == "man") {
         return 'H';
